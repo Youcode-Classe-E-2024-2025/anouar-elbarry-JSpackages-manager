4bb0df9bcc8931a3172ae8439c7aac26d1bac71b
anouar el barry <elbarryanwar37@gmail.com>
2024-12-08 15:49:20 +0100

    Include Necessary Files

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'database/config.php';
+require_once 'src/controllers/authorController.php';
+require_once 'src/controllers/packageController.php';
+require_once 'src/controllers/versionController.php';
+?>
