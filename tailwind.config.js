bdae4257f25649e9d3f81b9132fe80272b8cb03f
anouar el barry <elbarryanwar37@gmail.com>
2024-12-08 15:46:07 +0100

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","src/views/listVersions.html","src/views/listPackages.html","src/views/listAuthors.html",
+    "src/viewsa/ddVersion.html","src/views/addPackage.html","src/views/addAuthor.html","src/public/scripts.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
