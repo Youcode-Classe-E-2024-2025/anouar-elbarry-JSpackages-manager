ae29609da0ea0c49939506cea71f55fd1d66b6ac
anouar el barry <elbarryanwar37@gmail.com>
2024-12-09 15:33:58 +0100

    change input name from name to username

--- index.php
+++ index.php
@@ -43,10 +43,10 @@ require_once 'src/controllers/versionController.php';
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="process-login.php" method="POST">
+              <form class="space-y-4 md:space-y-6" action="./process-login.php" method="POST">
                   <div>
-                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User name</label>
-                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="user name" required="">
+                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User name</label>
+                      <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="user name" required="">
                   </div>
                   <div>
                       <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -63,7 +63,7 @@ require_once 'src/controllers/versionController.php';
                       </div>
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <button type="submit" class="w-full text-black bg-blue-600 border hover:bg-blue-950 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                       click <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">here</a> to return back 
                   </p>
