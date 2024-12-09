0785678ec886240854ccbc6ba31bb5b7cc6009f7
anouar el barry <elbarryanwar37@gmail.com>
2024-12-09 12:38:16 +0100

    convert files from html into php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'database/config.php';
-require_once 'src/controllers/authorController.php';
 require_once 'src/controllers/packageController.php';
 require_once 'src/controllers/versionController.php';
 ?>
