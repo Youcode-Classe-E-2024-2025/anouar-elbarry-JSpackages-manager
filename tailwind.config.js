0f8213f10049e6bc7f51ca5230915c242b544304
anouar el barry <elbarryanwar37@gmail.com>
2024-12-09 11:23:08 +0100

    add login page to the configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}","./index.php","src/views/listVersions.html","src/views/listPackages.html","src/views/listAuthors.html",
-    "src/viewsa/ddVersion.html","src/views/addPackage.html","src/views/addAuthor.html","src/public/scripts.js"
+    "src/viewsa/ddVersion.html","src/views/addPackage.html","src/views/addAuthor.html","src/public/scripts.js","./login.php"
   ],
   theme: {
     extend: {},
