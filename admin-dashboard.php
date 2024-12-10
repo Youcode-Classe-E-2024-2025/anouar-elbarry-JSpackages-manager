a70db40487755d71146e88c2cde71345542fdb58
anouar el barry <elbarryanwar37@gmail.com>
2024-12-10 12:29:56 +0100

    creat table for versions and relate it to the database

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -276,7 +276,64 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
             </tbody>
         </table>
     </div>
+<!-- Versions table -->
+<div class="hidden mt-14 versions_table h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                     ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                     Version_number
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                     Release_date
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                     Package_id
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM versions";
+                $result = $conn->query($sql);
+                if (!$result) {
+                    die("Invalid query:" . $conn->error);
+                }
 
+                //read data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "
+                     <tr
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+                    <th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                        $row[id]
+                    </th>
+                    <td class='px-6 py-4'>
+                      
+                        $row[version_number]
+                    </td>
+                    <td class='px-6 py-4'>
+                        $row[release_date]
+                    </td>
+                    <td class='px-6 py-4'>
+                        $row[package_id]
+                    </td>
+                    <td class='px-6 py-4 text-right'>
+                        <a href='#' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+                    </td>
+                </tr>
+                    ";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 <!-- script -->
     <script>
