8993cce93693fb86fcc5c55d2a0690de50cca55c
anouar el barry <elbarryanwar37@gmail.com>
2024-12-10 12:25:38 +0100

    creat table for packages and relate it to the database / creat script to hidde and show it (table)

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -154,6 +154,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
           </a>
         </div>
 </div>
+<!-- Authors table -->
 <div class="hidden mt-14 authors_table h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -211,17 +212,88 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
             </tbody>
         </table>
     </div>
+<!-- Packages table -->
+<div class="hidden mt-14 packages_table h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Description
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Created At
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Authors_Id
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM packages";
+                $result = $conn->query($sql);
+                if (!$result) {
+                    die("Invalid query:" . $conn->error);
+                }
 
+                //read data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "
+                     <tr
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+                    <th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                        $row[id]
+                    </th>
+                    <td class='px-6 py-4'>
+                      
+                        $row[name]
+                    </td>
+                    <td class='px-6 py-4'>
+                        $row[description]
+                    </td>
+                    <td class='px-6 py-4'>
+                        $row[created_at]
+                    </td>
+                    <td class='px-6 py-4'>
+                        $row[auteurs_id]
+                    </td>
+                    <td class='px-6 py-4 text-right'>
+                        <a href='#' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+                    </td>
+                </tr>
+                    ";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+
+<!-- script -->
     <script>
       const versions = document.getElementById('versions');
       const packages = document.getElementById('packages');
       const authors = document.getElementById('authors');
 
       const authorsTable = document.querySelector('.authors_table');
+      const packagesTable = document.querySelector('.packages_table');
       const controllCards = document.querySelector('.controll_cards');
 
       authors.addEventListener('click',()=> {
              authorsTable.classList.toggle('hidden');
+             controllCards.classList.toggle('hidden');
+      })
+      packages.addEventListener('click',()=> {
+             packagesTable.classList.toggle('hidden');
              controllCards.classList.toggle('hidden');
       })
     </script>
