e095ce7fc0758971b27ea1c0f56e16e2580e821e
anouar el barry <elbarryanwar37@gmail.com>
2024-12-10 22:10:20 +0100

    insert data into versions table

--- database/schema.sql
+++ database/schema.sql
@@ -31,3 +31,15 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user', 'author') NOT NULL
 );
+
+
+
+INSERT INTO versions (version_number, release_date, package_id) VALUES
+('16.13.1', '2020-03-19', 1), -- React
+('17.0.2', '2021-03-22', 1), -- React
+('1.1.3', '2017-03-25', 2),  -- Axios
+('1.3.0', '2018-05-15', 2),  -- Axios
+('2.2.19', '2022-02-10', 4), -- Tailwind CSS
+('3.3.3', '2023-04-15', 4),  -- Tailwind CSS
+('26.6.3', '2020-09-01', 1), -- Jest
+('27.5.1', '2022-01-10', 1); -- Jest
