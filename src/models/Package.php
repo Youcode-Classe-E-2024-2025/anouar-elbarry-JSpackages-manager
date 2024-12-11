6c554238eb9addbe084c3c2afe5db8103f34ff6d
anouar el barry <elbarryanwar37@gmail.com>
2024-12-11 13:00:24 +0100

    get author name data from the database

--- src/models/Package.php
+++ src/models/Package.php
@@ -1,4 +1,13 @@
-<div class="hidden mt-14 addAuthor-modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
+<?php
+require_once 'database/config.php';
+
+session_start();
+if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
+    header('Location: index.php');
+    exit;
+}
+?>
+<div class="hidden mt-14 Addpackage-modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">Package Information</h2>
         <form action="/submit_author" method="POST">
             <div class="mb-4">
@@ -19,7 +28,19 @@
             <div class="mb-4">
                 <label for="Authorname" class="block text-gray-700 font-medium mb-2">Authors name</label>
                 <select name="Authorname" id="Authorname">
-                    <option value=""></option>
+                    <?php
+                $sql = "SELECT Author_name FROM auteurs";
+                $result = $conn->query($sql);
+                if (!$result) {
+                    die("Invalid query:" . $conn->error);
+                }
+                //read data of each row
+                while ($option = $result->fetch_assoc()) {
+                    echo "
+                   <option id='$option[Author_name]' class='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' required>$option[Author_name]</option>
+                    ";
+                }
+                ?>
                 </select>
             </div>
 
