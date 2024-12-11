77cc7faecea0cc8abb0c110b2b9359b8f549fd08
anouar el barry <elbarryanwar37@gmail.com>
2024-12-11 14:58:39 +0100

    creat form modal for versions

---
+++ src/models/Version.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'database/config.php';
+
+session_start();
+if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
+    header('Location: index.php');
+    exit;
+}
+?>
+<div class="hidden mt-14 Addpackage-modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
+        <h2 class="text-2xl font-bold mb-4">version Information</h2>
+        <form action="/submit_author" method="POST">
+            <div class="mb-4">
+                <label for="name" class="block text-gray-700 font-medium mb-2">Version_number</label>
+                <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+            </div>
+            <div class="mb-4">
+                <label for="date" class="block text-gray-700 font-medium mb-2">Release_date</label>
+                <input type="date" id="date" name="date" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+            </div>
+
+            <div class="mb-4">
+                <label for="Authorname" class="block text-gray-700 font-medium mb-2">package name</label>
+                <select name="Authorname" id="Authorname"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <option value="choose an Author" selected>choose a package</option>
+                    <?php
+                $sql = "SELECT package_name FROM packages";
+                $result = $conn->query($sql);
+                if (!$result) {
+                    die("Invalid query:" . $conn->error);
+                }
+                //read data of each row
+                while ($option = $result->fetch_assoc()) {
+                    echo "
+                   <option id='$option[package_name]'>$option[package_name]</option>
+                    ";
+                }
+                ?>
+                </select>
+            </div>
+
+            <div class="flex justify-end">
+                <button type="submit" class="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600">Submit</button>
+            </div>
+        </form>
+    </div>    
