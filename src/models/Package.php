1f2484e2c6be0035b408b6816a74eb2e6ba320b4
anouar el barry <elbarryanwar37@gmail.com>
2024-12-11 15:06:27 +0100

    remove session_start();

--- src/models/Package.php
+++ src/models/Package.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'database/config.php';
 
-session_start();
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
     header('Location: index.php');
     exit;
