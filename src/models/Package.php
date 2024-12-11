5a2bd1bdffcab6da117e7c83c112ff83bfb36c10
anouar el barry <elbarryanwar37@gmail.com>
2024-12-11 17:02:44 +0100

    change the path of action form

--- src/models/Package.php
+++ src/models/Package.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
 ?>
 <div class="hidden mt-14 Addpackage-modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">Package Information</h2>
-        <form action="/submit_author" method="POST">
+        <form action="src\controllers\packageController.php" method="POST">
             <div class="mb-4">
                 <label for="name" class="block text-gray-700 font-medium mb-2">Name</label>
                 <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
