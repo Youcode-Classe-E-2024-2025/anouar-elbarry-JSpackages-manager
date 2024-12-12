c33fe8a39b8d6af9fd4b4cbfa88946a601015ca7
anouar el barry <elbarryanwar37@gmail.com>
2024-12-12 19:46:23 +0100

    add all modals in the configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./index.php","src/views/listVersions.html","src/views/listPackages.html","src/views/listAuthors.html",
+  content: ["./src/**/*.{html,js}","./index.php","src/models/Author.php","src/models/Package.php","src/models/version.php",
     "src/viewsa/ddVersion.html","src/views/addPackage.html","src/views/addAuthor.html","src/public/scripts.js","admin-dashboard.php","author-dashboard.php","user-dashboard.php"
   ],
   theme: {
