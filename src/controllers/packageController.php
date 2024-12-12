6f69d00cc7be14d567dca7424838c71499a53de3
anouar el barry <elbarryanwar37@gmail.com>
2024-12-12 10:02:13 +0100

    fix the proplem of set the data into the database

--- src/controllers/packageController.php
+++ src/controllers/packageController.php
@@ -1,7 +1,5 @@
 <?php 
-session_start();
 require_once './../../database/config.php';
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve form data
     $name = $_POST['name'];
@@ -19,15 +17,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "SELECT id FROM auteurs WHERE Author_name = '$Authorname'";
     $resultId = $conn->query($sql);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
     if ($resultId->num_rows > 0) {
         // Author exists, fetch the ID
         $row = $resultId->fetch_assoc();
         $authorId = $row['id'];
 
         // Insert data into the table
-        $insertSql = "INSERT INTO auteurs (package_name, pack_description, created_at, auteurs_id) 
+        $insertSql = "INSERT INTO packages (package_name, pack_description, created_at, auteurs_id) 
                       VALUES ('$name', '$bio', '$date', $authorId)";
-       
+       if ($conn->query($insertSql) === TRUE) {
+    echo "Package added successfully!";
+} else {
+    echo "Error: " . $insertSql . "<br>" . $conn->error;
+}
     } 
 }
 ?>
