93147aaeb79ae430c69aaca2bb88eaa55c79f5f2
anouar el barry <elbarryanwar37@gmail.com>
2024-12-12 10:49:19 +0100

    add action to version form

--- src/models/Version.php
+++ src/models/Version.php
@@ -8,19 +8,19 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
 ?>
 <div class="hidden mt-14 Addversion-modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">version Information</h2>
-        <form action="/submit_author" method="POST">
+        <form action="src\controllers\versionController.php" method="POST">
             <div class="mb-4">
-                <label for="name" class="block text-gray-700 font-medium mb-2">Version_number</label>
-                <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="Version_number" class="block text-gray-700 font-medium mb-2">Version_number</label>
+                <input type="text" id="Version_number" name="Version_number" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4">
-                <label for="date" class="block text-gray-700 font-medium mb-2">Release_date</label>
-                <input type="date" id="date" name="date" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="Release_date" class="block text-gray-700 font-medium mb-2">Release_date</label>
+                <input type="date" id="Release_date" name="Release_date" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
 
             <div class="mb-4">
-                <label for="Authorname" class="block text-gray-700 font-medium mb-2">package name</label>
-                <select name="Authorname" id="Authorname"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="package_name" class="block text-gray-700 font-medium mb-2">package name</label>
+                <select name="package_name" id="package_name"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                 <option value="choose an Author" selected>choose a package</option>
                     <?php
                 $sql = "SELECT package_name FROM packages";
