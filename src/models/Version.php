aeec2ebe1e34a62a4506bc336608b679e08897c0
anouar el barry <elbarryanwar37@gmail.com>
2024-12-13 16:52:31 +0100

    change action path

--- src/models/Version.php
+++ src/models/Version.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
 ?>
 <div class="hidden mt-14 Addversion-modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">version Information</h2>
-        <form action="src\controllers\versionController.php" method="POST">
+        <form action="./../src/controllers/versionController.php" method="POST">
             <div class="mb-4">
                 <label for="Version_number" class="block text-gray-700 font-medium mb-2">Version_number</label>
                 <input type="text" id="Version_number" name="Version_number" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
